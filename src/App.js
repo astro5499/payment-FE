cb464c32de98bfecbf2e6e81a8b7a3f7f7c331c0
Tran Duc Thinh <thinhtran@274@gmail.com>
2025-10-31 02:25:42 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//import PaymentPage from "./page/PaymentPage";
+import PaymentPage from "./page/PaymentPage";
 import KobsendDashboard from "./page/KobsendDashboard";
 import PaymentSuccess from "./page/KobsendDashboard";
 
@@ -7,6 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
+          <Route path="/payment" element={<PaymentPage />} />
         <Route path="/" element={<KobsendDashboard />} />
         <Route path="/payment-success" element={<PaymentSuccess />} />
       </Routes>
